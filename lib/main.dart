36cb652c7488d32a6297764f385cc475f9e9818e
Delta-Psi <delta.psi00@gmail.com>
2020-11-16 03:03:47 +0100

    database (?)

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-void main() => runApp(MyApp());
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
-class Tasks extends StatefulWidget {
-    @override
-    _TasksState createState() => _TasksState();
+import 'dart:io';
+
+class Task {
+    final int id;
+    final String name;
+
+    Task({this.id, this.name});
+
+    Map<String, dynamic> toMap() {
+        return {
+            'id': id,
+            'name': name,
+        };
+    }
 }
 
-class _TasksState extends State<Tasks> {
+class DatabaseHelper {
+    static Database _db;
+
+    Future<Database> get db async {
+        if (_db == null) _db = await initDb();
+        return _db;
+    }
+
+    Future<Database> initDb() async {
+        WidgetsFlutterBinding.ensureInitialized();
+        return await openDatabase(
+            join(await getDatabasesPath(), 'tasks.db'),
+            onCreate: (db, version) {
+                print('creating database');
+                return db.execute(
+                    "CREATE TABLE Tasks(name TEXT)",
+                );
+            },
+            version: 2,
+        );
+    }
+
+    Future<Task> createTask(name) async {
+        var db_ = await db;
+
+        final int id = await db_.insert(
+            'Tasks',
+            {'name': name},
+        );
+
+        return Task(id: id, name: name);
+    }
+
+    Future<List<Task>> listTasks() async {
+        final Database db_ = await db;
+
+        final List<Map<String, dynamic>> maps = await db_.query(
+            'Tasks',
+            columns: ['rowid', 'name'],
+        );
+
+        return List.generate(maps.length, (i) {
+            return Task(id: maps[i]['rowid'], name: maps[i]['name']);
+        });
+    }
+}
+
+void main() {
+    runApp(MyApp());
+}
+
+class TaskEditor extends StatefulWidget {
+    @override
+    _TaskEditorState createState() => _TaskEditorState();
+}
+
+class _TaskEditorState extends State<TaskEditor> {
+    final DatabaseHelper db = DatabaseHelper();
+
     final _formKey = GlobalKey<FormState>();
     final _textController = TextEditingController();
-    List<String> _tasks = [];
 
-    void _addTask(task) {
+    void _addTask(name) {
         setState(() {
-            _tasks.add(task);
+            db.createTask(name);
         });
     }
 
     void _removeTaskAt(index) {
-        setState(() {
-            _tasks.removeAt(index);
-        });
+        //setState(() {
+            //_tasks.removeAt(index);
+        //});
     }
 
     @override
@@ -58,23 +128,33 @@ class _TasksState extends State<Tasks> {
                 ),
                 Divider(),
                 Expanded(
-                    child: ListView.builder(
-                        itemCount: _tasks.length,
-                        itemBuilder: (BuildContext context, int index) {
-                            return Container(
-                                height: 50,
-                                child: Row(
-                                    children: [
-                                        Expanded(child: Text('${_tasks[index]}')),
-                                        ElevatedButton(
-                                            onPressed: () {
-                                                _removeTaskAt(index);
-                                            },
-                                            child: Text('Remove'),
-                                        ),
-                                    ],
-                                ),
-                            );
+                    child: FutureBuilder<List<Task>>(
+                        future: db.listTasks(),
+                        builder: (BuildContext context, AsyncSnapshot<List<Task>> tasks) {
+                            if (tasks.hasData) {
+                                return ListView.builder(
+                                    itemCount: tasks.data.length,
+                                    itemBuilder: (BuildContext context, int index) {
+                                        return Container(
+                                            height: 50,
+                                            child: Row(
+                                                children: [
+                                                    Text('${tasks.data[index].id}'),
+                                                    Expanded(child: Text('${tasks.data[index].name}')),
+                                                    ElevatedButton(
+                                                        onPressed: () {
+                                                            _removeTaskAt(index);
+                                                        },
+                                                        child: Text('Remove'),
+                                                    ),
+                                                ],
+                                            ),
+                                        );
+                                    },
+                                );
+                            } else {
+                                return Text('...');
+                            }
                         },
                     ),
                 ),
@@ -89,14 +169,16 @@ class MyApp extends StatelessWidget {
         return MaterialApp(
             title: 'Time Manager',
             home: DefaultTabController(
-                length: 2,
+                length: 3,
                 child: Scaffold(
                     appBar: AppBar(
+                        backgroundColor: Color(0xFF457285),
                         title: Text('Time Manager'),
                         bottom: TabBar(
                             tabs: [
                                 Tab(icon: Icon(Icons.edit)),
                                 Tab(icon: Icon(Icons.access_time)),
+                                Tab(icon: Icon(Icons.article)),
                             ],
                         ),
                     ),
@@ -105,9 +187,10 @@ class MyApp extends StatelessWidget {
                         children: [
                             Padding(
                                 padding: EdgeInsets.symmetric(horizontal: 10.0),
-                                child: Tasks(),
+                                child: TaskEditor(),
                             ),
                             Text('fuck'),
+                            Text('yeah'),
                         ],
                     ),
                 ),
